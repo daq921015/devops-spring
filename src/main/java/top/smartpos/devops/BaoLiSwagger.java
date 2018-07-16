8ff129640c979bec23a85ecefddc518003c12cc8
liubanglong <liubanglong@smartpos.top>
2018-07-16 18:49:02 +0800

    devops-spring  swagger 啦啦啦

--- src/main/java/top/smartpos/devops/BaoLiSwagger.java
+++ src/main/java/top/smartpos/devops/BaoLiSwagger.java
@@ -3,11 +3,13 @@ package top.smartpos.devops;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
-import springfox.documentation.builders.*;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -15,7 +17,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -23,24 +24,6 @@ import java.util.Set;
 public class BaoLiSwagger {
     @Bean
     public Docket createRestApi_baoli() {
-        //保利公共参数
-        Parameter service = new ParameterBuilder().name("service").description("要访问的接口名称").modelRef(new ModelRef("string")).parameterType("query").required(true).build();
-        Parameter appkey = new ParameterBuilder().name("appkey").description("应用号。由汉朔提供。").modelRef(new ModelRef("string")).parameterType("query").required(true).build();
-        Parameter merchant_id = new ParameterBuilder().name("merchant_id").description("商户ID:商家/平台渠道").modelRef(new ModelRef("int")).parameterType("query").required(true).build();
-        Parameter _input_charset = new ParameterBuilder().name("_input_charset").defaultValue("utf-8").description("参数编码字符集,暂仅支持utf-8。").modelRef(new ModelRef("string")).parameterType("query").required(true).build();
-        Parameter sing_type = new ParameterBuilder().name("sing_type").defaultValue("MD5").description("签名方式,暂仅支持MD5。").modelRef(new ModelRef("string")).parameterType("query").required(true).build();
-        Parameter sign = new ParameterBuilder().name("sign").description("签名").modelRef(new ModelRef("string")).parameterType("query").required(true).build();
-        Parameter cw_ca_request = new ParameterBuilder().name("cw_ca_request").description("签名类型(1:证书签名2:其他密钥签名.如果为空,默认为2)").modelRef(new ModelRef("string")).parameterType("query").required(false).build();
-        //公共参数列表
-        List<Parameter> aParameters = new ArrayList<Parameter>() {{
-            add(service);
-            add(appkey);
-            add(merchant_id);
-            add(_input_charset);
-            add(sing_type);
-            add(sign);
-            add(cw_ca_request);
-        }};
         //公共响应状态
         ArrayList<ResponseMessage> responseMessages = new ArrayList<ResponseMessage>() {{
             add(new ResponseMessageBuilder().code(200).message("成功").build());
@@ -50,8 +33,9 @@ public class BaoLiSwagger {
         Set<String> protocolsSet = new LinkedHashSet<String>() {{
             add("http");
         }};
-        return new Docket(DocumentationType.SWAGGER_2).groupName("保利会员系统").apiInfo(apiInfo_baoli())
-                .globalOperationParameters(aParameters)
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("保利会员系统")
+                .apiInfo(apiInfo_baoli())
                 .globalResponseMessage(RequestMethod.GET, responseMessages)
                 .globalResponseMessage(RequestMethod.POST, responseMessages)
                 .protocols(protocolsSet)
--- src/main/java/top/smartpos/devops/controllers/baoli/BaoLiController.java
+++ src/main/java/top/smartpos/devops/controllers/baoli/BaoLiController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.smartpos.devops.annotations.ApiRestAction;
 import top.smartpos.devops.beans.Models.baoli.GetVipInfoModel;
@@ -12,15 +13,13 @@ import top.smartpos.devops.beans.protocol.Result;
 import top.smartpos.devops.services.DEnvService;
 
 @RestController
-//@RequestMapping(value = "/baoli")
+@RequestMapping(value = "/baoli")
 @Api(tags = "保利接口")
 public class BaoLiController {
     @PostMapping(value = "/getVipInfo")
     @ApiOperation(value = "获取会员信息", notes = "根据会员手机号、卡号、动态会员号等查询会员信息,三者必须传其一")
-    @ApiImplicitParams({@ApiImplicitParam(name = "cellphone", value = "手机号(11~20位整数)"),
-            @ApiImplicitParam(name = "card", value = "卡号")})
     @ApiRestAction(modelClass = GetVipInfoModel.class, serviceClass = DEnvService.class, methodName = "findAll")
-    public Result getVipInfo() {
+    public Result getVipInfo(GetVipInfoModel getVipInfoModel) {
         return null;
     }
 }
